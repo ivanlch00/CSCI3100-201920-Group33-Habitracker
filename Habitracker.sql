44572783fec2b39daf14855b68827427b235b65a
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-04-13 16:33:06 +0800

    Add files via upload

--- Habitracker.sql
+++ Habitracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 13, 2020 at 10:15 AM
+-- Generation Time: Apr 13, 2020 at 10:32 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -93,7 +93,13 @@ INSERT INTO `chat_message` (`chat_message_id`, `to_user_id`, `from_user_id`, `ch
 (2, 2, 1, 'hello', '2020-03-26 17:39:51', 1),
 (3, 4, 1, 'hello', '2020-03-26 17:44:10', 0),
 (4, 1, 4, 'hi', '2020-03-26 17:47:29', 0),
-(5, 4, 1, 'hey', '2020-03-26 17:58:28', 1);
+(5, 4, 1, 'hey', '2020-03-26 17:58:28', 1),
+(6, 6, 1, 'Lets go to gym tgt xd!!!!', '2020-04-13 08:27:46', 0),
+(7, 1, 6, 'Go!!! Where do you live???', '2020-04-13 08:27:46', 0),
+(8, 1, 6, 'I am in MongKok let\'s go to the public gym!', '2020-04-13 08:27:46', 0),
+(9, 2, 7, 'Let\'s learn spanish tgt?!', '2020-04-13 08:27:46', 0),
+(10, 7, 2, 'Good I need someone to motivate me', '2020-04-13 08:27:46', 0),
+(11, 2, 7, 'I will be on Zoom!', '2020-04-13 08:27:46', 0);
 
 -- --------------------------------------------------------
 
@@ -128,7 +134,20 @@ INSERT INTO `goals` (`username`, `goal_id`, `goal_name`, `goal_description`, `go
 ('wck', 13, 'breakfast', '', '', '2020-04-29', '08:00:00', '09:30:00', 0, 0),
 ('wck', 14, '3100', '3100', '', '2020-04-23', '22:00:00', '23:00:00', 0, 1),
 ('wck', 16, '3100', '', '', '2020-04-14', '05:00:00', '06:00:00', 0, 1),
-('wck', 17, '3100', '', '', '2020-04-14', '10:00:00', '20:20:00', 1, 0);
+('wck', 17, '3100', '', '', '2020-04-14', '10:00:00', '20:20:00', 1, 0),
+('PikachuMaster', 46, 'Espanolll', 'Spainish netflix everyday', '', '2020-03-06', '20:00:00', '21:00:00', 1, 0),
+('PikachuMaster', 47, 'Cook', 'Everyday cook breakfast', '', '2020-03-07', NULL, NULL, 0, 0),
+('CaptainAmerica', 48, 'DC plan', 'Everyday watch one Batman Movie', '', '2020-02-14', '00:14:07', '00:23:04', 1, 0),
+('CaptainAmerica', 49, 'Biceps plan', 'Everyday go to gym', 'Help girlfriend with errands', '2020-02-15', '00:00:07', '00:12:24', 1, 0),
+('CaptainAmerica', 50, 'Learn to use Swift', 'Watch YT tutorial', 'Write notes', '2020-02-12', '00:00:07', '00:27:24', 0, 0),
+('AndyTsang', 51, 'Travel plan', 'Everyday go to the Victoria Harbour', 'Find a girlfriend asap', '2020-02-14', NULL, NULL, 0, 0),
+('AndyTsang', 52, 'Biceps training', 'Everyday go to gym', 'Help mama with errands', '2020-01-23', NULL, NULL, 1, 0),
+('Ivan118', 53, 'Sleeping plan', 'Everyday sleep for 4 hours or more', 'Read Kindle before sleep', '2020-02-25', NULL, NULL, 1, 0),
+('Mary223', 54, 'Lose weight', 'Go to gym', 'Not eat cakes', '2020-01-15', NULL, NULL, 1, 0),
+('Mary223', 55, 'Get a boyfriend', 'Practise makeup', '', '2020-02-14', NULL, NULL, 0, 0),
+('PeterLol', 56, 'Write a novel', 'ONe week 6 pages', '', '2020-03-04', '00:00:17', '00:22:24', 1, 0),
+('PeterLol', 57, 'Learn spanish', 'Go to duolinguo', '', '2020-02-19', '00:00:47', '00:12:34', 1, 0),
+('John23', 58, 'Biceps!!!', 'Everyday go to gym', 'You need a gf next year', '2020-02-13', '00:00:01', '00:04:24', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -155,7 +174,14 @@ INSERT INTO `login` (`user_id`, `username`, `email`, `password`, `image_status`,
 (1, 'wck', 'wongchika@ymail.com', '$2y$10$UOyrRT.86mhGtI9wqqJL.OhZeD/uFiz2Lfnv.eyRd9XuKOFIsYaw.', 1, 'Chi Ka', 'Wong', 'Hello!!!'),
 (2, 'phoebe', 'phoebechan0209@gmail.com', '$2y$10$sT2rf8RkA663a6TkLnYfiOr8JLybofKP4rQ0tojMXrjNgAuJYD.la', 1, NULL, NULL, NULL),
 (3, 'chika', 'wongchika26@gmail.com', '$2y$10$FlFTxLUMIl2LtTSPMqzVbe/5nM1ynVQrqoOm/M9v2XXCMJto8Xlyq', 1, NULL, NULL, NULL),
-(4, 'cst', 'phoebe@gmail.com', '$2y$10$1s/oPYE2nNcjBTtCRXGX3.r35S.ui6dymZ4L2/Oh1gAiiwnuiqIGu', 1, NULL, NULL, NULL);
+(4, 'cst', 'phoebe@gmail.com', '$2y$10$1s/oPYE2nNcjBTtCRXGX3.r35S.ui6dymZ4L2/Oh1gAiiwnuiqIGu', 1, NULL, NULL, NULL),
+(5, 'John23', 'john234@gmail.com', '2347893john', 1, 'John', 'Chan', 'Welcome to my page!'),
+(6, 'PeterLol', 'peterrwong@gmail.com', '2n3rfpeter', 1, 'Peter', 'Wong', 'Nice to meet you all!'),
+(7, 'Mary223', 'marychan@gmail.com', 'h3n4corona', 1, 'Mary', 'Lee', 'Let\'s reach our goals together!'),
+(8, 'Ivan118', 'ivan1144@gmail.com', '135ivan345', 1, 'Ivan', 'Lai', 'Nice to meet you!'),
+(9, 'AndyTsang', 'andy667t4@gmail.com', 'andyyts114', 1, 'Andy', 'Tsang', 'Keep going!'),
+(10, 'CaptainAmerica', 'capa2020@gmail.com', 'tonystark3000', 1, 'Captain', 'America', 'Captain Cmerica is the best!'),
+(11, 'PikachuMaster', 'ashhketchamp@gmail.com', 'iwannacatchtemall23', 1, 'Pikachu', 'Master', 'Let\'s go catch pikachu!');
 
 -- --------------------------------------------------------
 
@@ -280,19 +306,19 @@ ALTER TABLE `activity_users_list`
 -- AUTO_INCREMENT for table `chat_message`
 --
 ALTER TABLE `chat_message`
-  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `goals`
 --
 ALTER TABLE `goals`
-  MODIFY `goal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `goal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `login_details`
