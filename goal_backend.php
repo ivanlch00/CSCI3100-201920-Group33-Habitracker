17c000c4dfeed2bb5f7536f31af0108de8a29730
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-04-13 15:59:32 +0800

    Add files via upload

--- goal_backend.php
+++ goal_backend.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    require 'header.php';
     if( !isset( $_SESSION['username']) ){
         echo "You are not authorized to view this page. Go back <a href= '/'>home</a>";
         exit();
@@ -12,18 +12,17 @@
             $goal_subtask = $_POST['goal_subtask'];
             $username = $_SESSION['username'];
             $goal_enddate = date("Y-m-d", strtotime("+{$_POST['duration']} days"));
-            $goal_starttime = "{$_POST['goal_starttime_hh']}:{$_POST['goal_starttime_mm']}:00";
-            $goal_endtime = "{$_POST['goal_endtime_hh']}:{$_POST['goal_endtime_mm']}:00";
+            if (($_POST['goal_starttime_hh'])&&($_POST['goal_starttime_mm'])) {$goal_starttime = "{$_POST['goal_starttime_hh']}:{$_POST['goal_starttime_mm']}:00";} else {$goal_starttime = NULL;};
+            if (($_POST['goal_endtime_hh'])&&($_POST['goal_endtime_mm'])) {$goal_endtime = "{$_POST['goal_endtime_hh']}:{$_POST['goal_endtime_mm']}:00";} else {$goal_endtime = NULL;};
             $goal_public = (isset($_POST['goal_public'])) ? 1 : 0;
             //sanitize your input
             $goal_name = mysqli_real_escape_string($conn, $goal_name);
             $goal_description = mysqli_real_escape_string($conn, $goal_description);
             $goal_subtask = mysqli_real_escape_string($conn, $goal_subtask);
             
-            $sql = "Insert Into goals (username, goal_name, goal_description, goal_subtask, goal_enddate, goal_starttime, goal_endtime, goal_public) VALUES ('$username', '$goal_name', '$goal_description', '$goal_subtask', '$goal_enddate', '$goal_starttime', '$goal_endtime', '$goal_public')";
-            $sql = $conn->query($sql);
+            $sql = "Insert Into goals (username, goal_name, goal_description, goal_subtask, goal_enddate, goal_starttime, goal_endtime, goal_public, goal_completed) VALUES ('$username', '$goal_name', '$goal_description', '$goal_subtask', '$goal_enddate', ".($goal_starttime==NULL ? "NULL" : "'$goal_starttime'").", ".($goal_endtime==NULL ? "NULL" : "'$goal_endtime'").", '$goal_public', 0)";
             if($sql){
-                echo "Goal creation successful. You may <a href=mygoals.php>view your goal list</a> now";
+                header("Location: ../Habitracker/mygoals.php?create_goal=success");
             }
         } else if (isset($_POST['edit_goal'])) {
             $goal_id = $_SESSION['goal_id'];
@@ -32,21 +31,38 @@
             $goal_subtask = $_POST['goal_subtask'];
             $username = $_SESSION['username'];
             $goal_enddate = date("Y-m-d", strtotime("+{$_POST['duration']} days"));
-            $goal_starttime = "{$_POST['goal_starttime_hh']}:{$_POST['goal_starttime_mm']}:00";
-            $goal_endtime = "{$_POST['goal_endtime_hh']}:{$_POST['goal_endtime_mm']}:00";
+            if (($_POST['goal_starttime_hh'])&&($_POST['goal_starttime_mm'])) {$goal_starttime = "{$_POST['goal_starttime_hh']}:{$_POST['goal_starttime_mm']}:00";} else {$goal_starttime = NULL;};
+            if (($_POST['goal_endtime_hh'])&&($_POST['goal_endtime_mm'])) {$goal_endtime = "{$_POST['goal_endtime_hh']}:{$_POST['goal_endtime_mm']}:00";} else {$goal_endtime = NULL;};
             $goal_public = (isset($_POST['goal_public'])) ? 1 : 0;
             //sanitize your input
             $goal_name = mysqli_real_escape_string($conn, $goal_name);
             $goal_description = mysqli_real_escape_string($conn, $goal_description);
             $goal_subtask = mysqli_real_escape_string($conn, $goal_subtask);
             
-            $sql = "Update goals Set goal_name = '$goal_name', goal_description = '$goal_description',goal_subtask = '$goal_subtask',goal_enddate = '$goal_enddate',goal_starttime = '$goal_starttime',goal_endtime = '$goal_endtime',goal_public = '$goal_public' Where goal_id = '$goal_id'";
+            $sql = "Update goals Set goal_name = '$goal_name', goal_description = '$goal_description',goal_subtask = '$goal_subtask',goal_enddate = '$goal_enddate',goal_starttime = ".($goal_starttime==NULL ? "NULL" : "'$goal_starttime'").",goal_endtime = ".($goal_endtime==NULL ? "NULL" : "'$goal_endtime'").",goal_public = '$goal_public' Where goal_id = '$goal_id'";
             $sql = $conn->query($sql);
-            if($sql){
-                echo "Goal updated. You may <a href=mygoals.php>view your goal list</a> now";
-            }
-        } else if (isset($_POST['delete_goal'])){
             
+            if($sql){
+                header("Location: ../Habitracker/mygoals.php?edit_goal=success");
+            }
+        } else if (isset($_POST['update_goal_completion'])) {
+            $username = $_SESSION['username'];
+            $today = date("Y-m-d", time());
+            $sql = "Select * from goals Where username = '$username' and goal_enddate >= '$today'";
+            $search_result = $conn->query($sql);
+            
+            if ($search_result->num_rows >0) {
+                while($row = $search_result->fetch_assoc()) {
+                    $goal_id = $row['goal_id'];
+                    $goal_completed = (isset($_POST["goal_completed_$goal_id"])) ? 1 : 0;
+                    $sql = "Update goals Set goal_completed = '$goal_completed' Where goal_id = '$goal_id'";
+                    $sql = $conn->query($sql);
+                }
+            }
+            
+            if($sql){
+                header("Location: ../Habitracker/goal_progress_today.php?update_goal_completion=success");
+            }
         }
     }else{
         header('location: index.php');
