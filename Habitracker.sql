94d88dc7e991763fb88d48c3615d8c148497ba51
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-04-21 22:41:14 +0800

    Update Habitracker.sql

--- Habitracker.sql
+++ Habitracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 20, 2020 at 04:38 PM
+-- Generation Time: Apr 21, 2020 at 04:40 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -143,7 +143,7 @@ INSERT INTO `chat_message` (`chat_message_id`, `to_user_id`, `from_user_id`, `ch
 (2, 2, 1, 'hello', '2020-03-26 17:39:51', 1),
 (3, 4, 1, 'hello', '2020-03-26 17:44:10', 0),
 (4, 1, 4, 'hi', '2020-03-26 17:47:29', 0),
-(5, 4, 1, 'hey', '2020-03-26 17:58:28', 1),
+(5, 4, 1, 'hey', '2020-03-26 17:58:28', 0),
 (6, 6, 1, 'Lets go to gym tgt xd!!!!', '2020-04-13 08:27:46', 0),
 (7, 1, 6, 'Go!!! Where do you live???', '2020-04-13 08:27:46', 0),
 (8, 1, 6, 'I am in MongKok let\'s go to the public gym!', '2020-04-13 08:27:46', 0),
@@ -151,6 +151,41 @@ INSERT INTO `chat_message` (`chat_message_id`, `to_user_id`, `from_user_id`, `ch
 (10, 7, 2, 'Good I need someone to motivate me', '2020-04-13 08:27:46', 0),
 (11, 2, 7, 'I will be on Zoom!', '2020-04-13 08:27:46', 0),
 (12, 2, 1, 'hello', '2020-04-19 16:40:02', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL COMMENT 'primary key',
+  `employee_name` varchar(255) NOT NULL COMMENT 'employee name',
+  `employee_salary` double NOT NULL COMMENT 'employee salary',
+  `employee_age` int(11) NOT NULL COMMENT 'employee age'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='datatable demo table';
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `employee_name`, `employee_salary`, `employee_age`) VALUES
+(1, 'Tiger Nixon', 320800, 61),
+(2, 'Garrett Winters', 170750, 63),
+(3, 'Ashton Cox', 86000, 66),
+(4, 'Cedric Kelly', 433060, 22),
+(5, 'Airi Satou', 162700, 33),
+(6, 'Brielle Williamson', 372000, 61),
+(7, 'Herrod Chandler', 137500, 59),
+(8, 'Rhona Davidson', 327900, 55),
+(9, 'Colleen Hurst', 205500, 39),
+(10, 'Sonya Frost', 103600, 23),
+(11, 'Jena Gaines', 90560, 30),
+(12, 'Quinn Flynn', 342000, 22),
+(13, 'Charde Marshall', 470600, 36),
+(14, 'Haley Kennedy', 313500, 43),
+(15, 'Tatyana Fitzpatrick', 385750, 19),
+(16, 'Michael Silva', 198500, 66);
 
 -- --------------------------------------------------------
 
@@ -169,38 +204,38 @@ CREATE TABLE `goals` (
   `goal_endtime` time DEFAULT NULL,
   `goal_public` tinyint(1) NOT NULL,
   `goal_completed` tinyint(1) NOT NULL DEFAULT 0,
-  `streak` int(11) NOT NULL DEFAULT 0
+  `streak` int(11) NOT NULL DEFAULT 0,
+  `streak_lastSun` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `goals`
 --
 
-INSERT INTO `goals` (`username`, `goal_id`, `goal_name`, `goal_description`, `goal_subtask`, `goal_enddate`, `goal_starttime`, `goal_endtime`, `goal_public`, `goal_completed`, `streak`) VALUES
-('wck', 4, 'Make a database', '', '', '2020-04-30', NULL, NULL, 0, 0, 0),
-('wck', 5, 'Make a chat box', 'goal_description', '', '0000-00-00', NULL, NULL, 0, 0, 0),
-('wck', 6, 'Make a nice looking interface', '', '', '0000-00-00', NULL, NULL, 1, 0, 0),
-('wck', 7, 'I guess this should be correct', 'a description that is not blank', 'a sub-task', '2020-04-29', NULL, NULL, 0, 0, 0),
-('wck', 8, 'Public goal', '', '', '0000-00-00', NULL, NULL, 0, 0, 0),
-('wck', 9, 'public goal', '', '', '0000-00-00', NULL, NULL, 1, 0, 0),
-('wck', 13, 'breakfast', '', '', '2020-04-29', '08:00:00', '09:30:00', 0, 0, 0),
-('wck', 14, '3100', '3100', '', '2020-04-23', '22:00:00', '23:00:00', 0, 0, 0),
-('wck', 16, '3100', '', '', '2020-04-14', '05:00:00', '06:00:00', 0, 0, 0),
-('wck', 17, '3100', '', '', '2020-04-14', '10:00:00', '20:20:00', 1, 0, 0),
-('PikachuMaster', 46, 'Espanolll', 'Spainish netflix everyday', '', '2020-03-06', '20:00:00', '21:00:00', 1, 0, 0),
-('PikachuMaster', 47, 'Cook', 'Everyday cook breakfast', '', '2020-03-07', NULL, NULL, 0, 0, 0),
-('CaptainAmerica', 48, 'DC plan', 'Everyday watch one Batman Movie', '', '2020-02-14', '00:14:07', '00:23:04', 1, 0, 0),
-('CaptainAmerica', 49, 'Biceps plan', 'Everyday go to gym', 'Help girlfriend with errands', '2020-02-15', '00:00:07', '00:12:24', 1, 0, 0),
-('CaptainAmerica', 50, 'Learn to use Swift', 'Watch YT tutorial', 'Write notes', '2020-02-12', '00:00:07', '00:27:24', 0, 0, 0),
-('AndyTsang', 51, 'Travel plan', 'Everyday go to the Victoria Harbour', 'Find a girlfriend asap', '2020-02-14', NULL, NULL, 0, 0, 0),
-('AndyTsang', 52, 'Biceps training', 'Everyday go to gym', 'Help mama with errands', '2020-01-23', NULL, NULL, 1, 0, 0),
-('Ivan118', 53, 'Sleeping plan', 'Everyday sleep for 4 hours or more', 'Read Kindle before sleep', '2020-02-25', NULL, NULL, 1, 0, 0),
-('Mary223', 54, 'Lose weight', 'Go to gym', 'Not eat cakes', '2020-01-15', NULL, NULL, 1, 0, 0),
-('Mary223', 55, 'Get a boyfriend', 'Practise makeup', '', '2020-02-14', NULL, NULL, 0, 0, 0),
-('PeterLol', 56, 'Write a novel', 'ONe week 6 pages', '', '2020-03-04', '00:00:17', '00:22:24', 1, 0, 0),
-('PeterLol', 57, 'Learn spanish', 'Go to duolinguo', '', '2020-02-19', '00:00:47', '00:12:34', 1, 0, 0),
-('John23', 58, 'Biceps!!!', 'Everyday go to gym', 'You need a gf next year', '2020-02-13', '00:00:01', '00:04:24', 1, 0, 0),
-('wck', 72, 'Running', '', '', '2020-04-18', NULL, NULL, 1, 0, 0);
+INSERT INTO `goals` (`username`, `goal_id`, `goal_name`, `goal_description`, `goal_subtask`, `goal_enddate`, `goal_starttime`, `goal_endtime`, `goal_public`, `goal_completed`, `streak`, `streak_lastSun`) VALUES
+('wck', 4, 'Make a database', '', '', '2020-04-30', NULL, NULL, 0, 0, 0, 0),
+('wck', 5, 'Make a chat box', 'goal_description', '', '0000-00-00', NULL, NULL, 0, 0, 0, 0),
+('wck', 6, 'Make a nice looking interface', '', '', '0000-00-00', NULL, NULL, 1, 0, 0, 0),
+('wck', 7, 'I guess this should be correct', 'a description that is not blank', 'a sub-task', '2020-04-29', NULL, NULL, 0, 0, 0, 0),
+('wck', 8, 'Public goal', '', '', '0000-00-00', NULL, NULL, 0, 0, 0, 0),
+('wck', 9, 'public goal', '', '', '0000-00-00', NULL, NULL, 1, 0, 0, 0),
+('wck', 13, 'breakfast', '', '', '2020-04-29', '08:00:00', '09:30:00', 0, 0, 0, 0),
+('wck', 16, '3100', '', '', '2020-04-14', '05:00:00', '06:00:00', 0, 0, 0, 0),
+('wck', 17, '3100', '', '', '2020-04-14', '10:00:00', '20:20:00', 1, 0, 0, 0),
+('PikachuMaster', 46, 'Espanolll', 'Spainish netflix everyday', '', '2020-03-06', '20:00:00', '21:00:00', 1, 0, 0, 0),
+('PikachuMaster', 47, 'Cook', 'Everyday cook breakfast', '', '2020-03-07', NULL, NULL, 0, 0, 0, 0),
+('CaptainAmerica', 48, 'DC plan', 'Everyday watch one Batman Movie', '', '2020-02-14', '00:14:07', '00:23:04', 1, 0, 0, 0),
+('CaptainAmerica', 49, 'Biceps plan', 'Everyday go to gym', 'Help girlfriend with errands', '2020-02-15', '00:00:07', '00:12:24', 1, 0, 0, 0),
+('CaptainAmerica', 50, 'Learn to use Swift', 'Watch YT tutorial', 'Write notes', '2020-02-12', '00:00:07', '00:27:24', 0, 0, 0, 0),
+('AndyTsang', 51, 'Travel plan', 'Everyday go to the Victoria Harbour', 'Find a girlfriend asap', '2020-02-14', NULL, NULL, 0, 0, 0, 0),
+('AndyTsang', 52, 'Biceps training', 'Everyday go to gym', 'Help mama with errands', '2020-01-23', NULL, NULL, 1, 0, 0, 0),
+('Ivan118', 53, 'Sleeping plan', 'Everyday sleep for 4 hours or more', 'Read Kindle before sleep', '2020-02-25', NULL, NULL, 1, 0, 0, 0),
+('Mary223', 54, 'Lose weight', 'Go to gym', 'Not eat cakes', '2020-01-15', NULL, NULL, 1, 0, 0, 0),
+('Mary223', 55, 'Get a boyfriend', 'Practise makeup', '', '2020-02-14', NULL, NULL, 0, 0, 0, 0),
+('PeterLol', 56, 'Write a novel', 'ONe week 6 pages', '', '2020-03-04', '00:00:17', '00:22:24', 1, 0, 0, 0),
+('PeterLol', 57, 'Learn spanish', 'Go to duolinguo', '', '2020-02-19', '00:00:47', '00:12:34', 1, 0, 0, 0),
+('John23', 58, 'Biceps!!!', 'Everyday go to gym', 'You need a gf next year', '2020-02-13', '00:00:01', '00:04:24', 1, 0, 0, 0),
+('wck', 72, 'Running', '', '', '2020-04-18', NULL, NULL, 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -272,7 +307,13 @@ INSERT INTO `login_details` (`login_details_id`, `user_id`, `last_activity`, `is
 (49, 2, '2020-04-20 13:58:36', 'no'),
 (50, 3, '2020-04-20 13:58:41', 'no'),
 (51, 4, '2020-04-20 13:58:47', 'no'),
-(52, 1, '2020-04-20 14:38:44', 'no');
+(52, 1, '2020-04-20 14:38:55', 'no'),
+(53, 1, '2020-04-20 14:57:59', 'no'),
+(54, 1, '2020-04-21 03:46:57', 'no'),
+(55, 1, '2020-04-21 08:38:03', 'no'),
+(56, 4, '2020-04-21 06:13:41', 'no'),
+(57, 1, '2020-04-21 08:43:20', 'no'),
+(58, 1, '2020-04-21 13:30:43', 'no');
 
 -- --------------------------------------------------------
 
@@ -315,13 +356,13 @@ CREATE TABLE `reports` (
 --
 
 INSERT INTO `reports` (`report_id`, `report_type`, `goal_id`, `activity_id`, `reason`, `report_time`, `resolved`, `goal_name`, `activity_name`, `deleted`, `dismissed`, `reporter`, `owner`) VALUES
-(1, 'goal', 14, NULL, 'IDK', '2020-04-14 12:35:25', 0, 'lunch', NULL, 0, 0, 'phoebe', 'wck'),
+(1, 'goal', 14, NULL, 'IDK', '2020-04-14 12:35:25', 1, '3100', NULL, 1, 0, 'phoebe', 'wck'),
 (2, 'goal', 17, NULL, 'I just want to delete this goal.', '2020-04-14 14:38:01', 0, '3100', '', 0, 0, 'phoebe', 'wck'),
-(3, 'activity', NULL, 4, 'xxx', '2020-04-14 15:03:42', 0, '', 'sPANISHH', 0, 0, 'wck', 'pikachu'),
+(3, 'activity', NULL, 4, 'xxx', '2020-04-14 15:03:42', 1, '', 'sPANISHH', 0, 1, 'wck', 'pikachu'),
 (4, 'activity', NULL, 1, 'test', '2020-04-14 15:04:48', 0, NULL, 'Golf', 0, 0, 'wck', 'pikachu'),
-(5, 'goal', 17, NULL, 'this goal is boring', '2020-04-14 12:35:25', 0, '3100', '', 0, 0, 'cst', 'wck'),
+(5, 'goal', 17, NULL, 'this goal is boring', '2020-04-14 12:35:25', 1, '3100', '', 0, 1, 'cst', 'wck'),
 (7, 'activity', NULL, 5, 'This activity is boring.', '2020-04-15 21:14:51', 0, NULL, 'german', 0, 0, 'lkh', 'pikachu'),
-(8, 'activity', NULL, 6, 'I don\'t like it', '2020-04-15 21:16:07', 0, NULL, 'Hiking', 0, 0, 'lkh', 'wck'),
+(8, 'activity', NULL, 6, 'I don\'t like it', '2020-04-15 21:16:07', 0, NULL, 'Hiking', 1, 0, 'lkh', 'wck'),
 (9, 'goal', 48, NULL, 'I like Marvel', '2020-04-15 21:19:21', 0, 'DC plan', NULL, 0, 0, 'lkh', 'CaptainAmerica');
 
 --
@@ -351,6 +392,12 @@ ALTER TABLE `activity_users_list`
 --
 ALTER TABLE `chat_message`
   ADD PRIMARY KEY (`chat_message_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `goals`
@@ -411,6 +458,12 @@ ALTER TABLE `chat_message`
   MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key', AUTO_INCREMENT=64;
+
+--
 -- AUTO_INCREMENT for table `goals`
 --
 ALTER TABLE `goals`
@@ -426,7 +479,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `login_details`
 --
 ALTER TABLE `login_details`
-  MODIFY `login_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `login_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `pwdreset`
