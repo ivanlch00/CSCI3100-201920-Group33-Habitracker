e9c719004c526d9530963ec44ddc9d449290ecad
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-04-23 10:42:01 +0800

    update index page

--- index.php
+++ index.php
@@ -1,18 +1,81 @@
-<?php
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+
+    <body>
+
+    <?php
     require "header.php";
 ?>
 
 
     <main>
         <?php
+        if (isset($_GET['login'])){
+            if($_GET['login'] == 'success')
+                echo "<p>You are logged in! </p>";
+        }
             if (isset($_SESSION['user_id'])){
-                echo '<p>You are logged in! You can access different functions of Habitracker using links at the header. Explore your journey in Habitracker!</p>';
+                echo '<p>You can access different functions of Habitracker using links at the header. Explore your journey in Habitracker!</p>';
             }
             else {
                 echo '<p>You are not logged in! Please log in to use Habitracker.</p>';
             }
         ?>
     </main>
+
+
+        <header>
+            <div class="content">
+                <div class="kolom">
+                    <div class="atas">
+                        <a href="create_goal.php">
+                            <img src="img/target.png">
+                        </a>
+                    </div>
+                    <div class="tengah">
+                        <h2>GOALS</h2>
+                    </div>
+                    <div class="bawah">
+                        <p>Set up a goal to step out of your comfort zone and challenge yourself - you might discover something beyond your imagination!</p>
+                    </div>
+                </div>
+                <div class="kolom">
+                    <div class="atas">
+                        <a href="activity_create_nonrecurring.php">
+                            <img src="img/bicycling.png">
+                        </a>
+                    </div>
+                    <div class="tengah">
+                        <h2>ACTIVITES</h2>
+                    </div>
+                    <div class="bawah">
+                        <p>Start an activity and find some hobby-buddies - meet new friends and be one another's support in this exploration!</p></div>
+                    </div>
+                <div class="kolom">
+                    <div class="atas">
+                        <a href="chatindex.php">
+                            <img src="img/conversation.png">
+                        </a>
+                    </div>
+                    <div class="tengah">
+                        <h2>CHAT</h2>
+                    </div>
+                    <div class="bawah">
+                        <p>Talk to friends when you are in doubt - share your experience or get some advice from the supportive user community!</p>
+                    </div>
+                </div>
+                
+
+            </div>
+        </header>
+
+
+    </body>
+
+
+
 
 <?php
     require "footer.php";
