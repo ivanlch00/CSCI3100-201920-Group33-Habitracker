cbad5e0749928decad58ebe45d40a984f3b72725
ivanlch00 <63771128+ivanlch00@users.noreply.github.com>
2020-05-16 23:04:53 +0800

    Update activity_display_details.php

--- activity_display_details.php
+++ activity_display_details.php
@@ -41,7 +41,7 @@
         echo '<div><h1>Activity details</h1></div>';
         
         $activityID = $_GET['id'];
-        
+         //getting the activity id so that we can query it in the table
         $conn = mysqli_connect("localhost","root","","Habitracker");
         $sql = "SELECT * FROM activity_table WHERE activity_id = ".$activityID." ";
         $result = mysqli_query($conn,$sql);
@@ -58,15 +58,17 @@
     if(!empty($row['activity_one_off_datetime'])){
         $date= $row['activity_one_off_datetime'];
         echo "<div></br>Date and time: ".$date."</div>";
-        //echo "<div><label for='date'>Activity Date:</label> <input type='text' id='date' value='.$date.'> </div>";
+        //displaying the date and time
     }
     if($row['activity_repetition']==1){
+       //for once a week
         
         echo "<div><br>The activity will be held once a week on ".
         getProperWeekDay($row['activity_recurring_date_0']).
         " at ".$row['activity_recurring_time_0'].". </div>";
     }
     if($row['activity_repetition']==2){
+       //for twice a week
         
         echo "<div><br>The activity will be held twice a week on ".
         getProperWeekDay($row['activity_recurring_date_0']).
@@ -77,6 +79,7 @@
         " </div>";
     }
     if($row['activity_repetition']==3){
+       //for thrice a week
         
         echo "<div><br>The activity will be held three times a week on ".
         getProperWeekDay($row['activity_recurring_date_0']).
@@ -100,6 +103,7 @@
 
 <?php
     $sql_2 = "SELECT * FROM activity_users_list WHERE activity_id = ".$row['activity_id']." ";
+           //to find the user name to display it in the table, we need to look up the name in the user_list with the corresponding acitivity id
     $result_2 = mysqli_query($conn,$sql_2);
     if ($result_2->num_rows > 0) {
         echo '<ul>';
